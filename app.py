266a059c6d84324d4fcafd829e6195d397c37f8d
waleeed2025 <waliwaleed9876@gmail.com>
2025-05-21 00:50:45 +0500

    Update app.py

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ def send_message():
     label { color: white; }
     .file { height: 30px; }
     body {
-      background-image: url('https://i.pinimg.com/736x/86/03/c8/8603c8807932035290b6e2c800a11bd6.jpg');
+      background-image: url('https://i.pinimg.com/736x/50/23/e5/5023e520078707c4347a628688f237db.jpg');
       background-size: cover;
       background-repeat: no-repeat;
       color: white;
